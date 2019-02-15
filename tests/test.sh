adbbfe1c2181ce891f1a44950989eba472a67caf
Daniel Ly <nalply@gmail.com>
2019-02-15 10:07:30 +0100

    Removed older tests

--- tests/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $(dirname "$0")
-
-rm -f /tmp/asa-spectrum
-./genwaves.jl > /tmp/asa.pcm
-ASA_DBG=T ../asa-s16le -b 100 /tmp/asa.pcm /tmp/asa-spectrum 2> /tmp/asa.log
-./showspectrum.jl -b 100 < /tmp/asa-spectrum
